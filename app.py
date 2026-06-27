e53476401a75349304c438a683042e3e72b670e6
ankud996 <ankud996@gmail.com>
2026-06-28 01:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from ta.momentum import RSIIndicator
 app = Flask(__name__)
 
 BOT_TOKEN = "8971900274:AAGZVWOioaCkAJ3DuM_RAYT-MgoRxu9vavM"
-CHAT_ID = "1249990076"
+CHAT_ID = "-1002632083710"
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
